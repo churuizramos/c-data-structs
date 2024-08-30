5b493b7d361bb824b3ffe44061663680165f9c2b
Jesus Ruiz Ramos <ruizramos04@outlook.com>
2024-08-30 14:14:11 -0400

    developing the load function

--- week1/RuizRamos_Jesus_1/ClassSales.cpp
+++ week1/RuizRamos_Jesus_1/ClassSales.cpp
@@ -4,36 +4,51 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
 using namespace std;
 
 void addSale(int * &arrayPtr, int &size, int &classId, int &sales);
 void printSalesList(int * &arrayPtr, int &size);
+void printSales(int * &arrayPtr, int &classId);
+void saveSales(int * &arrayPtr, int &size);
+void loadSales(int * &arrayPtr, int &size);
 void displayIntro();
 
 int main() {
 	int size = 10;
-	int * arrayPtr = new int[size];
+	int * salesPtr = new int[size];
 	
 	bool running = true;
 
 	displayIntro();
 
 	while (running) {
-		cout << "Actions List\n1 Add Sale | 2 Print All Class Data | 3 Print Sales | 4 Save Sales | 5 Load Sales | 6 Exit" << endl;
+		cout << "Actions List\n1 Add Sale | 2 Print All Class Data | 3 Print Sales | 4 Save Sales | 5 Load Sales | 6 Exit : ";
 		int input;
-		cout << ":" << endl;
 		cin >> input;
 		switch (input) {
 			case 1:
 				int classId, salesAmount;
-				cout << "Class Id (1-10): " << endl;
+				cout << "Class Id (1-10): ";
 				cin >> classId;
-				cout << "Sales amount (<0): " << endl;
+				cout << "Sales amount (>0): ";
 				cin >> salesAmount;
-				addSale(arrayPtr, size, classId, salesAmount);
+				addSale(salesPtr, size, classId, salesAmount);
 				break;
 			case 2:
-				printSalesList(arrayPtr, size);
+				printSalesList(salesPtr, size);
+				break;
+			case 3:
+				int searchId;
+				cout << "Class Id (1-10): ";
+				cin >> searchId;
+				printSales(salesPtr, searchId);
+				break;
+			case 4:
+				saveSales(salesPtr, size);
+				break;
+			case 5:
+				loadSales(salesPtr, size);
 				break;
 			case 6:
 				running = false;
@@ -44,17 +59,18 @@ int main() {
 		}
 	}
 
-	cout << "end" << endl;
+	cout << "Closing program..." << endl;
 
 	return 0;
 }
 
 void addSale(int * &arrayPtr, int &size, int &classId, int &sales) {
-	if (classId < size) {
-		arrayPtr[classId] += sales;
+	int index = classId - 1;
+	if (index < size) {
+		arrayPtr[index] += sales;
 	}
 	else {
-		cout << "error" << endl;
+		cout << "Not within the range" << endl;
 	}
 
 }
@@ -62,8 +78,28 @@ void printSalesList(int * &arrayPtr, int &size) {
 	cout << "Current Sales" << endl;
 	cout << "=======================================" << endl;
 	for (int i = 0; i < size; ++i) {
-		cout << "Class: " << i << "\tSales: " << arrayPtr[i] << endl;
+		cout << "Class: " << i+1 << "\tSales: " << arrayPtr[i] << endl;
 	}
+}
+void printSales(int * &arrayPtr, int &classId) {
+	int index = classId - 1;
+	cout << "=======================================" << endl;
+	cout << "Class: " << classId << "\tSales: " << arrayPtr[index] << endl;
+	cout << "=======================================" << endl;
+}
+void saveSales(int * &arrayPtr, int &size) {
+	ofstream SaveFile("sales.out");
+	for (int i = 0; i < size; ++i) {
+		SaveFile << arrayPtr[i] << endl;
+	}
+	SaveFile.close();
+}
+void loadSales(int * &arrayPtr, int &size) {
+	ifstream ReadFile("sales.out");
+	for (int i = 0; i < size; ++i) {
+		arrayPtr[i] = ReadFile.getline();
+	}
+	ReadFile.close();
 }
 void displayIntro() {
 	cout << "Welcome to the class sales data manager" << endl;
