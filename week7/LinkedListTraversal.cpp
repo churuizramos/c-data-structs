1cfd05f7a5db0d381404e35d1a8eef0d01989c00
churuizramos <ruizramos04@outlook.com>
2024-10-11 22:50:46 -0400

    finished with this really good work

---
+++ week7/LinkedListTraversal.cpp
@@ -0,0 +1,36 @@
+#include <cstddef>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+class Node{
+    private:
+        int data;
+        Node * next;
+
+    public:
+        Node(int data) {
+            this->data = data;
+        }
+        void setNext(Node * next) {
+            this->next = next;
+        }
+        Node * getNext() {
+            return next;
+        }
+        int getData() {
+            return data;
+        }
+};
+
+int main () {
+    Node * head = new Node(12);
+    head->setNext(new Node(99));
+    head->getNext()->setNext(new Node(3));
+
+    cout << head->getData() << endl;
+    cout << head->getNext()->getData() << endl;
+    cout << head->getNext()->getNext()->getData() << endl;
+
+    return 0;
+}
