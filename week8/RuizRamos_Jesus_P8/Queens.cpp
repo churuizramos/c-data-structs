088c3d54e9a85c167131388ae6d2d05ac2a8f179
churuizramos <ruizramos04@outlook.com>
2024-10-18 18:16:16 -0400

    idk anymore

--- week8/RuizRamos_Jesus_P8/Queens.cpp
+++ week8/RuizRamos_Jesus_P8/Queens.cpp
@@ -1,53 +1,52 @@
+// Jesus Ruiz Ramos
+// 8 Queens Problem
+// Week 8 Programming Assignment
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
-bool isSafe(const vector<int>& board, int row, int col) {
-    for (int i = 0; i < row; ++i) {
-        if (board[i] == col || board[i] - i == col - row || board[i] + i == col + row) {
+const int N = 8;
+
+bool isSafe(vector<vector<int> >& board, int row, int col)
+{
+    for (int x = 0; x < col; x++)
+        if (board[row][x] == 1)
             return false;
-        }
-    }
+    for (int x = row, y = col; x >= 0 && y >= 0; x--, y--)
+        if (board[x][y] == 1)
+            return false;
+    for (int x = row, y = col; x < N && y >= 0; x++, y--)
+        if (board[x][y] == 1)
+            return false;
     return true;
 }
 
-bool solveNQueens(vector<int>& board, int row) {
-    int n = board.size();
-    if (row == n) {
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (board[i] == j) {
-                    cout << "Q";
-                }
-                else {
-                    cout << ".";
-                }
-            }
+bool queens(vector<vector<int> >& board, int col)
+{
+    if (col == N) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
+                cout << board[i][j] << " ";
             cout << endl;
         }
         cout << endl;
         return true;
     }
-
-    bool res = false;
-    for (int col = 0; col < n; ++col) {
-        if (isSafe(board, row, col)) {
-            board[row] = col;
-            res = solveNQueens(board, row+1) || res;
+    for (int i = 0; i < N; i++) {
+        if (isSafe(board, i, col)) {
+            board[i][col] = 1;
+            if (queens(board, col + 1))
+                return true;
+            board[i][col] = 0;
         }
     }
-    return res;
+    return false;
 }
 
-void solve() {
-    int n = 8;
-    vector<int> board(n, -1);
-    if (!solveNQueens(board, 0)) {
-        cout << "No solution exists." << endl;
-    }
-}
-
-int main() {
-    solve();
+int main()
+{
+    vector<vector<int> > board(N, vector<int>(N, 0));
+    queens(board, 0);
     return 0;
 }
